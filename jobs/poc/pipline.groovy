44cd88b11574c88f4f0586a8f508f22313707984
Maliszewski, Krystian <Krystian.Maliszewski@zuehlke.com>
2017-01-24 15:14:51 +0100

    add logstash config

--- jobs/poc/pipline.groovy
+++ jobs/poc/pipline.groovy
@@ -2,7 +2,8 @@ pipelineJob('pipline_one') {
     definition {
         cpsScm {
             scm {
-                git('https://github.com/jenkinsci/job-dsl-plugin.git')
+                
+                git('git@github.com:krystianmali/test_jenkins.git')
                 credentials('9fb70847-e508-466f-99c6-df3dadcfce93') 
             }
         }

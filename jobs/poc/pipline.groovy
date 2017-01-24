e7fd002634067e76bec828cf14c83452dec46466
Maliszewski, Krystian <Krystian.Maliszewski@zuehlke.com>
2017-01-24 18:07:21 +0100

    add key-store

--- jobs/poc/pipline.groovy
+++ jobs/poc/pipline.groovy
@@ -1,16 +1,18 @@
 pipelineJob('pipline_one') {
-      configure { project ->
-      flow-definition / publishers / 'jenkins.plugins.logstash.LogstashNotifier' {
-          maxLines(1000)
-          failBuild(false)
-      }
-    }
+      
 
     definition {
         cps {
             script("node{ sh 'ps' }" )
             sandbox()
         }
+    }
+
+    configure { project ->
+      flow-project / publishers / 'jenkins.plugins.logstash.LogstashNotifier' {
+          maxLines(1000)
+          failBuild(false)
+      }
     }
 
 }

089d00e9e46acca97cedca5d4f3fa0efe6438cd5
Maliszewski, Krystian <Krystian.Maliszewski@zuehlke.com>
2017-01-24 14:31:03 +0100

    add logstash config

--- jobs/poc/pipline.groovy
+++ jobs/poc/pipline.groovy
@@ -6,4 +6,10 @@ pipelineJob('pipline_one') {
             }
         }
     }
+    configure { project ->
+        project / publishers / 'jenkins.plugins.logstash.LogstashNotifier plugin="logstash@1.2.0"' {
+            maxLines(0)
+            failBuild(true)
+        }
+    }
 }
